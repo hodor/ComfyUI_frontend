01c5498dd90fa80d159c4e41f65eec455b1ed1b5
DrJKL <DrJKL0424@gmail.com>
2025-11-15 20:14:25 -0800

    design: Adding components to replace existing duplicates. See XKCD: standards

---
+++ src/components/ui/button-group/ButtonGroup.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+
+import { cn } from '@/utils/tailwindUtil'
+
+import type { ButtonGroupVariants } from './buttonGroupVariants'
+import { buttonGroupVariants } from './buttonGroupVariants'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+  orientation?: ButtonGroupVariants['orientation']
+}>()
+</script>
+
+<template>
+  <div
+    role="group"
+    data-slot="button-group"
+    :data-orientation="props.orientation"
+    :class="
+      cn(buttonGroupVariants({ orientation: props.orientation }), props.class)
+    "
+  >
+    <slot />
+  </div>
+</template>
